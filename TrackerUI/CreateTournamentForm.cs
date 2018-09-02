0adf5365e2277345ae3673fd2a9946b7d449160f
Asad Ajazi <AsadAjazi.work@gmail.com>
2018-09-02 21:22:49 +0100

    Add team button now populates teamlistbox correctly

--- TrackerUI/CreateTournamentForm.cs
+++ TrackerUI/CreateTournamentForm.cs
@@ -15,6 +15,8 @@ namespace TrackerUI
     public partial class CreateTournamentForm : Form
     {
         List<TeamModel> availableTeams = GlobalConfig.Connection.GetTeam_All();
+        List<TeamModel> selectedTeams = new List<TeamModel>();
+        List<PrizeModel> selectedPrizes = new List<PrizeModel>();
 
         public CreateTournamentForm()
         {
@@ -24,8 +26,36 @@ namespace TrackerUI
 
         private void PopulateList()
         {
+
+
+
+            selectTeamDropDown.DataSource = null;
             selectTeamDropDown.DataSource = availableTeams;
             selectTeamDropDown.DisplayMember = "TeamName";
+
+            tournamentTeamsListBox.DataSource = null;
+            tournamentTeamsListBox.DataSource = selectedTeams;
+            tournamentTeamsListBox.DisplayMember = "TeamName";
+
+            prizesListBox.DataSource = null;
+            prizesListBox.DataSource = selectedPrizes;
+            prizesListBox.DisplayMember = "PlaceName";
+        }
+
+        private void addTeamButton_Click(object sender, EventArgs e)
+        {
+
+            TeamModel team = (TeamModel)selectTeamDropDown.SelectedItem;
+            if (team != null)
+            {
+                availableTeams.Remove(team);
+                selectedTeams.Add(team);
+
+                PopulateList();
+            }
+
+
+
         }
     }
 }
